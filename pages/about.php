b1ddbfd9cf691eeaef6124820a49c9ebdf7ee01c
Dhairya K Singh <116647760+jaii-r-bhati@users.noreply.github.com>
2024-05-12 20:00:31 +0530

    Add files via upload

---
+++ pages/about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $title = "About";
+$active = "active";
+include "header.php" ?>
+
+<section id="services" class="services section-bg">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>About Team</h2>
+            <p>Teamwork is the ability to work together toward a common vision.</p>
+        </div>
+
+        <div class="row gy-4">
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                <div class="icon-box iconbox-green">
+                    <div class="icon" style="width: 160px; height: 160px;">
+                        <img src="CEO.png" class="img-round" alt="">
+                    </div>
+                    <h4><a href="">Dhairya k Singh</a></h4>
+                    <p class="mt-1"> <strong>(CEO)</strong></p>
+                    <p class="mt-3">The Man Who Has Power of Imagination, He Has a Wings and founder of AIT*BIF.</p>
+
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+                <div class="icon-box iconbox-blue ">
+                    <div class="icon" style="width: 160px; height: 160px;">
+                        <img src="DEVELOPER.png" class="img-round" alt="">
+                    </div>
+                    <h4><a href="">Ajender Rai</a></h4>
+                    <p class="mt-1"> <strong>(Designer & Developer)</strong></p>
+                    <p class="mt-3">Everything should be made as simple as possible, we are the future face of India.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+                <div class="icon-box iconbox-pink">
+                    <div class="icon" style="width: 160px; height: 160px;">
+                        <img src="FOUNDER.png" class="img-round" alt="">
+                    </div>
+                    <h4><a href="">Aditiya Raj</a></h4>
+                    <p class="mt-1"> <strong>(Founder)</strong></p>
+                    <p class="mt-3">Beauty in things exists in the mind which contemplates them, we believe in innovation.</p>
+                </div>
+            </div>         
+    </div>
+</section>
+
+<?php
+include "footer.php";
+?>
+
+
+</html>
